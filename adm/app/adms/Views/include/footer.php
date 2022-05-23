888dff9afe5dd1d892e94702edb9d365459faeef
marciovieira <marcio72vieira@hotmail.com>
2022-05-23 11:48:05 -0300

    Carregando o JQUERY e arquivo persolalizado js

--- adm/app/adms/Views/include/footer.php
+++ adm/app/adms/Views/include/footer.php
@@ -1,4 +1,6 @@
- </body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="<?php echo URLADM.'app/adms/assets/js/personalizado_adms.js'?>"></script>
+</body>
 </html>
 
 
